909fa7ef7b638fe88441b5f422cdb0857efd4b7c
Thomas Petig <thomas@petig.eu>
2025-05-23 00:27:48 +0200

    clean up and add progress bar

--- bin/sie2gnucash.py
+++ bin/sie2gnucash.py
@@ -7,33 +7,26 @@ import numpy as np
 from piecash import create_book, Account, Commodity, open_book, Transaction, Split
 from piecash.core.factories import create_currency_from_ISO
 from decimal import Decimal
+from tqdm import tqdm
 
 
 sie = pysie.PySIE ()
 sie.open_trans (sys.argv[2])
 sie.open (sys.argv[1])
 
-#pprint.pp(sie.verifikat)
-
 sek = create_currency_from_ISO("SEK")
 today = datetime.now()
 with open_book(sys.argv[3], readonly=False) as book:
-    print("###")
-    print("###")
-    print("###")
 
     SEK = book.currencies(mnemonic="SEK")
-    for k,v in sie.verifikat.items():
+    for k,v in tqdm(sie.verifikat.items()):
         d1 = datetime.strptime(v[0], "%Y%m%d")
         d2 = datetime.strptime(v[2], "%Y%m%d")
-        print(v)
         sp = []
         for ss in v[3]:
             a = book.accounts(code=ss[0])
             am = Decimal(ss[2])
             sp.append(Split(account=a, value=am))
-        print(sp)
         t=Transaction(post_date=d1.date(), enter_date=d2, currency=SEK, description = v[1], splits=sp, num=k)
-        print(t)
 
     book.save()
